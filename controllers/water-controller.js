0743ab22d6974f49515cf749f47a58dd03b55baf
Alina <alinatanst@gmail.com>
2024-04-01 10:24:30 +0300

    Validate date and count summary amount

--- controllers/water-controller.js
+++ controllers/water-controller.js
@@ -52,19 +52,51 @@ export const updateWaterController = async (req, res) => {
     const { id } = req.user
     const { id: waterId } = req.params;
     const { time, waterAmount } = req.body;
-    const {waters} = await User.findById(id);
+    const { waters } = await User.findById(id);
     const waterPortion = waters.find(water => water._id.toString() === waterId);
     if (!waterPortion) throw HttpError(404, `There are no portions with id ${waterId}`)
     
     if (!time && !waterAmount) throw HttpError(400, "Must be at least one field");
     if (time) waterPortion.time = time
-    if(waterAmount) waterPortion.amount = waterAmount
+    if (waterAmount) waterPortion.amount = waterAmount
 
     await User.findByIdAndUpdate(id, { $pull: { waters: { _id: waterId } } }, { new: true });
-    await User.findByIdAndUpdate(id, { $push: { waters: {...waterPortion}} }, { new: true })
-        return res.json({
-            "message": "Water portion is updated successful",
-            waterPortion
-        })
+    await User.findByIdAndUpdate(id, { $push: { waters: { ...waterPortion } } }, { new: true })
+    return res.json({
+        "message": "Water portion is updated successful",
+        waterPortion
+    })
+
+};
+
+export const getWaterInfoTodayController = async (req, res) => {
+    const { id } = req.user;
+    const { date } = req.body;
+    let sep
+    if (date.includes('/')) sep = '/'
+    else if (date.includes('-')) sep = '-'
+    const [day, month, year] = date.split(sep);
+    const { waterRate } = await User.findById(id)
+
+    const { waters } = await User.findById(id);
+    const watersInfo = waters.filter(water => water.year === year && water.month === months[Number(month) - 1] && water.day === day);
+    if(watersInfo.length === 0) return res.json({watersForDay: watersInfo})
+    const allAmountForDay = watersInfo.reduce((acc, water) => acc += water.amount, 0);
+    const dataAboutDay = {
+        day,
+        month: months[Number(month) - 1],
+        year,
+        waterRateForThisDay: waterRate,
+        allAmountForDay,
+    }
+
+    await User.findByIdAndUpdate(id, { $push: { watersForDay: dataAboutDay } }, { new: true });
+    return res.json({
+        watersForDay:
+        {
+            ...dataAboutDay,
+            part: `${allAmountForDay / (waterRate / 100)}%`
+        }
+    })
 
 }
--- routes/water-router.js
+++ routes/water-router.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { waterRateController, addWaterController, deleteWaterController, updateWaterController } from '../controllers/water-controller.js';
+import { waterRateController, addWaterController, deleteWaterController, updateWaterController, getWaterInfoTodayController } from '../controllers/water-controller.js';
 import { authValidation, isValidId } from '../middlewares/index.js'
 import { ctrlWrapper, validateBody } from '../helpers/index.js'
-import { waterAddedSchema, waterRateSchema, waterUpdateSchema } from "../models/userModel.js";
+import { waterAddedSchema, waterRateSchema, waterTodaySchema, waterUpdateSchema } from "../models/userModel.js";
 
 
 export const waterRouter = express.Router();
@@ -10,4 +10,7 @@ export const waterRouter = express.Router();
 waterRouter.patch('/water_rate', authValidation, validateBody(waterRateSchema), ctrlWrapper(waterRateController));
 waterRouter.put('/', authValidation, validateBody(waterAddedSchema), ctrlWrapper(addWaterController));
 waterRouter.put('/:id', authValidation, isValidId, validateBody(waterUpdateSchema), ctrlWrapper(updateWaterController));
-waterRouter.delete('/:id', authValidation, isValidId,  ctrlWrapper(deleteWaterController));
+waterRouter.delete('/:id', authValidation, isValidId, ctrlWrapper(deleteWaterController));
+
+waterRouter.get('/today', authValidation, validateBody(waterTodaySchema), ctrlWrapper(getWaterInfoTodayController));
+// waterRouter.get('/today', authValidation, validateBody(), ctrlWrapper(getWaterInfoMonthController));

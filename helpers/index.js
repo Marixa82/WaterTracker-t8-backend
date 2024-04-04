28c60fcdbaaa036663f644052ee1447277d3fea0
Visotsky Maxim <needlife124@gmail.com>
2024-04-05 00:43:25 +0300

    add rout forgot-password

--- helpers/index.js
+++ helpers/index.js
@@ -3,4 +3,4 @@ export { default as HttpError } from "./HttpError.js";
 export { default as ctrlWrapper } from "./ctrlWrapper.js";
 export { default as cloudinary } from "./cloudinary.js";
 export { default as sendEmail } from "./sendEmail.js";
-export { emailLetter } from "./emailLetter.js";
+export { emailVerify, emailTemporaryPassword } from "./emailLetter.js";
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,17 +1,37 @@
-
-import express from 'express';
-import { ctrlWrapper, validateBody } from '../helpers/index.js';
-import {loginController, logoutController, registerController, verifyEmailController,
-  resendVerifyEmailController, } from '../controllers/auth-controller.js';
-import { registerSchema, emailSchema } from '../models/userModel.js';
-import { authValidation } from '../middlewares/index.js';
+import express from "express";
+import { ctrlWrapper, validateBody } from "../helpers/index.js";
+import {
+  loginController,
+  logoutController,
+  registerController,
+  verifyEmailController,
+  resendVerifyEmailController,
+  forgotPasswordController,
+} from "../controllers/auth-controller.js";
+import { registerSchema, emailSchema } from "../models/userModel.js";
+import { authValidation } from "../middlewares/index.js";
 
 export const authRouter = express.Router();
 
 authRouter.get("/verify/:verificationCode", ctrlWrapper(verifyEmailController));
-authRouter.post("/verify",validateBody(emailSchema),ctrlWrapper(resendVerifyEmailController));
-authRouter.post('/register', validateBody(registerSchema), ctrlWrapper(registerController));
-authRouter.post('/login', validateBody(registerSchema), ctrlWrapper(loginController));
-authRouter.post('/logout', authValidation, ctrlWrapper(logoutController));
-
-
+authRouter.post(
+  "/verify",
+  validateBody(emailSchema),
+  ctrlWrapper(resendVerifyEmailController)
+);
+authRouter.post(
+  "/register",
+  validateBody(registerSchema),
+  ctrlWrapper(registerController)
+);
+authRouter.post(
+  "/login",
+  validateBody(registerSchema),
+  ctrlWrapper(loginController)
+);
+authRouter.post("/logout", authValidation, ctrlWrapper(logoutController));
+authRouter.get(
+  "/forgot-password",
+  validateBody(emailSchema),
+  ctrlWrapper(forgotPasswordController)
+);

5ed94e0427be5f09cab8e98307b686866754609d
Alina <alinatanst@gmail.com>
2024-04-04 11:06:21 +0300

    Added user-delete route

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ctrlWrapper, validateBody } from '../helpers/index.js';
-import { loginController, logoutController, registerController } from '../controllers/auth-controller.js';
+import { deleteController, loginController, logoutController, registerController } from '../controllers/auth-controller.js';
 import { registerSchema } from '../models/userModel.js';
 import { authValidation } from '../middlewares/index.js';
 
@@ -9,3 +9,4 @@ export const authRouter = express.Router();
 authRouter.post('/register', validateBody(registerSchema), ctrlWrapper(registerController));
 authRouter.post('/login', validateBody(registerSchema), ctrlWrapper(loginController));
 authRouter.post('/logout', authValidation, ctrlWrapper(logoutController));
+authRouter.delete('/', authValidation, validateBody(registerSchema), ctrlWrapper(deleteController))
